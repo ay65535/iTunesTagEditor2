cdbe74f4039d8292a64291d9e9ebd021f6c28bcf
ats <yasuda.atusi@gmail.com>
2014-08-02 22:23:04 +0900

    Initial Commit

---
+++ iTunesTagEditor2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iTunesTagEditor2
+//
+//  Created by Atsushi Yasuda on 2014/08/02.
+//  Copyright (c) 2014年 ay65535. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iTunesTagEditor2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  iTunesTagEditor2
+//
+//  Created by Atsushi Yasuda on 2014/08/02.
+//  Copyright (c) 2014年 ay65535. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
